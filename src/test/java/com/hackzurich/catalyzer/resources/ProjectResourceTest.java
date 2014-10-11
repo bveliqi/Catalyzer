baa6ff952cb07755d678d2eaa7c036dcb7967881
Behar <behar@veliqi.de>
2014-10-11 18:00:01 +0200

    Added a unit test

--- src/test/java/com/hackzurich/catalyzer/resources/ProjectResourceTest.java
+++ src/test/java/com/hackzurich/catalyzer/resources/ProjectResourceTest.java
@@ -1,7 +1,52 @@
 package com.hackzurich.catalyzer.resources;
 
+import com.hackzurich.catalyzer.api.Project;
+import com.hackzurich.catalyzer.jdbi.ProjectDao;
+import io.dropwizard.testing.junit.ResourceTestRule;
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import java.sql.Timestamp;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 /**
  * Created by behar on 11.10.14.
  */
 public class ProjectResourceTest {
+
+    private static final ProjectDao dao = mock(ProjectDao.class);
+    private Project project;
+
+    @ClassRule
+    public static final ResourceTestRule resources = ResourceTestRule.builder()
+            .addResource(new ProjectResource(dao))
+            .build();
+
+    @Before
+    public void setUp() throws Exception {
+        project = new Project();
+        project.setAuthorId(1);
+        project.setCategory("Enviroment");
+        project.setName("Clean the streets of Zürich");
+        project.setMotivation("For Fun");
+        project.setPhotoUrl("a/b/c.jpg");
+        project.setStatus("OK");
+        project.setStartDate(new Timestamp(DateTime.now().getMillis()));
+
+        when(dao.getById(eq(1L))).thenReturn(project);
+    }
+
+    @Test
+    public void testGetPersonById() throws Exception {
+        assertThat(resources.client().resource("/project/1").get(Project.class))
+                .isEqualTo(project);
+        verify(dao).getById(eq(1L));
+    }
 }
