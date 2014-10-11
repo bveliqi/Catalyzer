0057e5f2e7172bea6cd343e950ca2b5b5a7112eb
Behar <behar@veliqi.de>
2014-10-11 22:43:39 +0200

    Changed UserResource to calc SHA256 Digest before storing pwd

--- src/main/java/com/hackzurich/catalyzer/resources/UserResource.java
+++ src/main/java/com/hackzurich/catalyzer/resources/UserResource.java
@@ -7,7 +7,11 @@ import com.hackzurich.catalyzer.jdbi.UserDao;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 /**
@@ -17,10 +21,14 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
-    private final UserDao userDao;
+    private static final String SALT = "EA333B0BFC3DFFE85";
 
-    public UserResource(UserDao userDao) {
+    private final UserDao userDao;
+    final MessageDigest digestInstance;
+
+    public UserResource(UserDao userDao) throws NoSuchAlgorithmException {
         this.userDao = userDao;
+        digestInstance = MessageDigest.getInstance("SHA-256");
     }
 
 
@@ -29,7 +37,7 @@ public class UserResource {
     @Timed
     public User getById(@PathParam("id") long id) {
         final User user = userDao.getById(id);
-        if(user == null) {
+        if (user == null) {
             throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
         return user;
@@ -45,8 +53,19 @@ public class UserResource {
     @POST
     @Timed
     public Response insert(User user) {
+        String finalPassword = hashPassword(user);
+        user.setPassword(finalPassword);
         long id = userDao.insert(user);
         return Response.created(URI.create(String.valueOf(id))).build();
+    }
+
+    private String hashPassword(User user) {
+        String password = user.getPassword() == null ? "" : user.getPassword();
+        final byte[] bytes = password.getBytes(StandardCharsets.UTF_8);
+        digestInstance.update(bytes);
+        final byte[] digest = digestInstance.digest();
+        final String hashedPassword = new HexBinaryAdapter().marshal(digest);
+        return SALT + hashedPassword;
     }
 
 
@@ -54,7 +73,7 @@ public class UserResource {
     @Timed
     @Path("{id}/points/add/{points}")
     public void addPoints(@PathParam("id") long id, @PathParam("points") int points) {
-        if(userDao.getById(id) == null) {
+        if (userDao.getById(id) == null) {
             throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
         userDao.addPoints(id, points);
@@ -64,7 +83,7 @@ public class UserResource {
     @Timed
     @Path("{id}/points/remove/{points}")
     public void removePoints(@PathParam("id") long id, @PathParam("points") int points) {
-        if(userDao.getById(id) == null) {
+        if (userDao.getById(id) == null) {
             throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
         userDao.removePoints(id, points);
